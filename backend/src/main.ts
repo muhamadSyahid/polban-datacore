0af928bc92916152f7bbed4b086435cae194b538
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-10-28 10:45:59 +0700

    feat(backend): setup initial env
    Adds .env example, drizzle, pg, nestjs/config, and configures
    database connection.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
+const envpath = join(process.cwd(), '.env')
+
 async function bootstrap() {
+  dotenv.config({ path: envpath });
+
   const app = await NestFactory.create(AppModule);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
