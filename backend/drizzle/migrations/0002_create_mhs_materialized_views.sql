4b86a5d64cb46d5d2721d92f68c40a7797345c33
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-11-30 12:03:59 +0700

    refactor: change index to unique index on materialized views by Ikhsan Satriadi

--- backend/drizzle/migrations/0002_create_mhs_materialized_views.sql
+++ backend/drizzle/migrations/0002_create_mhs_materialized_views.sql
@@ -15,9 +15,9 @@ END) as "jenis", count(*)::int as "total" from "fact_mahasiswa" where "fact_maha
 END));--> statement-breakpoint
 CREATE MATERIALIZED VIEW "public"."mv_mahasiswa_domisili_kota" AS (select "nama_provinsi", "provinsi_lat", "provinsi_lng", "nama_wilayah", "wilayah_lat", "wilayah_lng", count(*)::int as "total" from "fact_mahasiswa" where "fact_mahasiswa"."nama_provinsi" is not null group by "fact_mahasiswa"."nama_provinsi", "fact_mahasiswa"."provinsi_lat", "fact_mahasiswa"."provinsi_lng", "fact_mahasiswa"."nama_wilayah", "fact_mahasiswa"."wilayah_lat", "fact_mahasiswa"."wilayah_lng");
 
-CREATE INDEX "mv_mhs_gender_angkatan_idx" ON "mv_mahasiswa_gender" ("angkatan");--> statement-breakpoint
-CREATE INDEX "mv_mhs_agama_angkatan_idx" ON "mv_mahasiswa_agama" ("angkatan");--> statement-breakpoint
-CREATE INDEX "mv_mhs_jalur_angkatan_idx" ON "mv_mahasiswa_jalur_daftar" ("angkatan");--> statement-breakpoint
-CREATE INDEX "mv_mhs_total_angkatan_idx" ON "mv_mahasiswa_total_per_angkatan" ("angkatan");--> statement-breakpoint
-CREATE INDEX "mv_mhs_slta_angkatan_idx" ON "mv_mahasiswa_slta_kategori" ("angkatan");--> statement-breakpoint
-CREATE INDEX "mv_mhs_domisili_provinsi_idx" ON "mv_mahasiswa_domisili_kota" ("nama_provinsi");--> statement-breakpoint
+CREATE UNIQUE INDEX "mv_mhs_gender_unique_idx" ON "mv_mahasiswa_gender" ("angkatan", "jenis_kelamin");--> statement-breakpoint
+CREATE UNIQUE INDEX "mv_mhs_agama_unique_idx" ON "mv_mahasiswa_agama" ("angkatan", "agama");--> statement-breakpoint
+CREATE UNIQUE INDEX "mv_mhs_jalur_unique_idx" ON "mv_mahasiswa_jalur_daftar" ("angkatan", "nama_jalur_daftar");--> statement-breakpoint
+CREATE UNIQUE INDEX "mv_mhs_total_angkatan_unique_idx" ON "mv_mahasiswa_total_per_angkatan" ("angkatan");--> statement-breakpoint
+CREATE UNIQUE INDEX "mv_mhs_slta_unique_idx" ON "mv_mahasiswa_slta_kategori" ("angkatan", "jenis");--> statement-breakpoint
+CREATE UNIQUE INDEX "mv_mhs_domisili_unique_idx" ON "mv_mahasiswa_domisili_kota" ("nama_provinsi", "nama_wilayah", "provinsi_lat", "provinsi_lng", "wilayah_lat", "wilayah_lng");
